ad2a8727dc8448d028009e3fa92eb358befe1f67
Lhara Medina <lharamaemedina@gmail.com>
2024-07-18 10:05:34 +0800

    feat: update acknowledgementReceipt--add position of qr and texts

--- src/app/acknowledgementReceipt/page.jsx
+++ src/app/acknowledgementReceipt/page.jsx
@@ -358,59 +358,22 @@ export default function Tesseract() {
 
     const pngImage = await newPdfDoc.embedPng(pngImageBytes);
     const pngDims = pngImage.scale(0.15);
-
+    
     // Get the dimensions of the first page or document
     const pageWidth = firstPage.getWidth();
     const pageHeight = firstPage.getHeight();
-
-    // Get CST Number
-    const textValue = OCRData[1].data.split(": ")[1];
-
-    // Embed text
-    // Normal font
-    // const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
-    // Bold font
-    const boldHelveticaFont = await newPdfDoc.embedFont(
-      StandardFonts.HelveticaBold
-    );
-
-    // ------------------------------------------------------------------------------------------
-
-    // Calculate the position to place the text in the upper right corner
-    const txtWidth = boldHelveticaFont.widthOfTextAtSize(textValue, 8);
-    const txtXMargin = 9;
-    const txtYMargin = 10;
-    const txtMargin = txtXMargin + txtYMargin;
-
-    const txtPosX = pageWidth - txtWidth - txtXMargin;
-    const txtPosY = txtYMargin;
-
-    newPdfDoc.getPages().map(async (page) => {
-      page.drawText(textValue, {
-        x: txtPosX,
-        y: txtPosY,
-        size: 8,
-        font: boldHelveticaFont,
-        color: rgb(0, 0, 0),
-      });
-    });
-
+    
     // Calculate the position to place the image in the lower right corner
-    // const imageWidth = pngDims.width;
-    // const imageHeight = pngDims.height;
     console.log("This is my image width/height", pngDims.width, pngDims.height);
-    const imageWidth = 31;
-    const imageHeight = 31;
-
-    // ---------------------------IMAGE HEIGHT/WIDTH -------------------------------
-
-    const imageXMargin = 10;
-    const imageYMargin = 18;
-    const imageMargin = imageXMargin + imageYMargin;
-
+    const imageWidth = 90;
+    const imageHeight = 90;
+    
+    const imageXMargin = 77;
+    const imageYMargin = 103;
+    
     const imagePosX = pageWidth - imageWidth - imageXMargin;
     const imagePosY = imageYMargin;
-
+    
     // Draw the image on the first page of the document
     firstPage.drawImage(pngImage, {
       x: imagePosX,
@@ -418,8 +381,61 @@ export default function Tesseract() {
       width: imageWidth,
       height: imageHeight,
     });
+    
+    // Get CTS Number
+    const textValue = OCRData[1].data.split(": ")[1];
+    // const textValue = `OCCEO-EGLD-415-${OCRData[1].data.split(": ")[1]}`;
+    const rectangleTitle = "In following-up, please cite CTS ref #";
+    
+    // Embed text
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+    const boldHelveticaFont = await newPdfDoc.embedFont(StandardFonts.HelveticaBold);
+    
+    // Calculate the position to place the rectangle title in the rectangle
+    const rectangleTitleWidth = helveticaFont.widthOfTextAtSize(rectangleTitle, 10);
+    const ctsTextWidth = boldHelveticaFont.widthOfTextAtSize(textValue, 10);
+    const rectangleWidth = Math.max(rectangleTitleWidth, ctsTextWidth) + 16; // Add margin
+    const rectangleHeight = 35;
+    
+    const qrCenterX = imagePosX + imageWidth / 2;
+    const rectanglePosX = qrCenterX - rectangleWidth / 2;
+    const rectanglePosY = imagePosY - 37;
+    
+    // rectangle
+    firstPage.drawRectangle({
+      x: rectanglePosX,
+      y: rectanglePosY,
+      width: rectangleWidth,
+      height: rectangleHeight,
+      borderColor: rgb(0, 0, 0),
+      borderWidth: 1,
+    });
+    
+    const rectangleTitlePosX = rectanglePosX + (rectangleWidth - rectangleTitleWidth) / 2;
+    const rectangleTitlePosY = rectanglePosY + 20;
 
-    // ------------------------------------------------------------------------------------------
+    // rectangle title
+    firstPage.drawText(rectangleTitle, {
+      x: rectangleTitlePosX,
+      y: rectangleTitlePosY,
+      size: 10,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+    
+    // cts text properties
+    const txtPosX = rectanglePosX + (rectangleWidth - ctsTextWidth) / 2; // Center the text within the rectangle
+    const txtPosY = rectanglePosY + 5;
+    
+    // cts text
+    firstPage.drawText(textValue, {
+      x: txtPosX,
+      y: txtPosY,
+      size: 10,
+      font: boldHelveticaFont,
+      color: rgb(0, 0, 0),
+    });
+     
 
     // Serialize the PDFDocument to bytes (a Uint8Array)
     const pdfBytes = await newPdfDoc.save();
@@ -429,22 +445,22 @@ export default function Tesseract() {
 
     //  Download feature
     // Create a URL for the Blob
-    const url = URL.createObjectURL(blob);
+    // const url = URL.createObjectURL(blob);
 
     // Create a temporary link element
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = "pdf-lib_modification_example.pdf";
+    // const link = document.createElement("a");
+    // link.href = url;
+    // link.download = "pdf-lib_modification_example.pdf";
 
     // Append the link to the body
-    document.body.appendChild(link);
+    // document.body.appendChild(link);
 
     // Trigger the download
-    link.click();
+    // link.click();
 
     // Clean up
-    URL.revokeObjectURL(url);
-    document.body.removeChild(link);
+    // URL.revokeObjectURL(url);
+    // document.body.removeChild(link);
     // End of download feature
 
     // PDF Viewer
